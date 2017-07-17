65920a666b113bc1c78051c766b1932a1739f455
dvg4000 <dvgrishechkin@yandex.ru>
2017-07-17 12:01:38 +0300

    Implement primary DetailsActivity.java

---
+++ garland-view-example/src/main/java/com/ramotion/garlandview/example/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.ramotion.garlandview.example;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+
+}
