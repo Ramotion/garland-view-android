9bfce7036ed442a6d5e38abfa1e2732a14121907
dvg4000 <dvgrishechkin@yandex.ru>
2017-08-31 11:42:13 +0300

    Add primary ProfileActivity header animation

--- garland-view-example/src/main/java/com/ramotion/garlandview/example/details/ProfileActivity.java
+++ garland-view-example/src/main/java/com/ramotion/garlandview/example/details/ProfileActivity.java
@@ -9,10 +9,14 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.ramotion.garlandview.example.R;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -46,7 +50,15 @@ public class ProfileActivity extends AppCompatActivity {
 
             final View headerImage = findViewById(R.id.header_image);
             final View headerInfo = findViewById(R.id.header_info);
+            final View avatar = findViewById(R.id.avatar_border);
+            final LinearLayout texts = (LinearLayout) findViewById(R.id.texts);
 
+            final int avatarHOffset = getResources().getDimensionPixelSize(R.dimen.profile_avatar_h_offset);
+            final int avatarVOffset = getResources().getDimensionPixelSize(R.dimen.profile_avatar_v_offset);
+            final int avatarSize = getResources().getDimensionPixelSize(R.dimen.profile_avatar_size);
+            final int textVMinOffset = getResources().getDimensionPixelSize(R.dimen.profile_texts_v_min_offset);
+            final int textVMaxOffset = getResources().getDimensionPixelSize(R.dimen.profile_texts_v_max_offset);
+            final int textVDiff = textVMaxOffset - textVMinOffset;
             final int headerHalf = getResources().getDimensionPixelSize(R.dimen.profile_header_half);
             final int toolBarHeight;
 
@@ -55,7 +67,14 @@ public class ProfileActivity extends AppCompatActivity {
                         new int[] { android.R.attr.actionBarSize });
                 toolBarHeight = (int) styledAttributes.getDimension(0, 0) + getStatusBarHeight();
                 styledAttributes.recycle();
+
+                avatar.setPivotX(0);
+                avatar.setPivotY(0);
+                texts.setPivotX(0);
+                texts.setPivotY(0);
             }
+
+            final ArrayList<Float> textStart = new ArrayList<>();
 
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
@@ -63,6 +82,37 @@ public class ProfileActivity extends AppCompatActivity {
                 final int y = diff < 0 ? headerHalf - diff : headerHalf;
                 headerImage.setBottom(y);
                 headerInfo.setTop(y);
+
+                final int totalScrollRange = appBarLayout.getTotalScrollRange();
+                final float ratio = ((float)totalScrollRange + verticalOffset) / totalScrollRange;
+
+                final int avatarHalf = avatar.getMeasuredHeight() / 2;
+                final int avatarRightest = appBarLayout.getMeasuredWidth() / 2 - avatarHalf - avatarHOffset;
+                final int avatarTopest = avatarHalf + avatarVOffset;
+
+                avatar.setX(avatarHOffset + avatarRightest * ratio);
+                avatar.setY(avatarVOffset - avatarTopest * ratio);
+                avatar.setScaleX(0.5f + 0.5f * ratio);
+                avatar.setScaleY(0.5f + 0.5f * ratio);
+
+                if (textStart.isEmpty() && verticalOffset == 0) {
+                    for (int i = 0; i < texts.getChildCount(); i++) {
+                        textStart.add(texts.getChildAt(i).getX());
+                        Log.d("D", String.format("i: %d, x: %f", i, textStart.get(i)));
+                    }
+                }
+
+                texts.setY(textVMinOffset + textVDiff * ratio);
+                texts.setScaleX(0.8f + 0.2f * ratio);
+                texts.setScaleY(0.8f + 0.2f * ratio);
+                for (int i = 0; i < textStart.size(); i++) {
+                    texts.getChildAt(i).setX(Math.max(
+                            avatarSize * 0.5f + avatarHOffset * 3,
+                            textStart.get(i) * ratio));
+                }
+
+                Log.d("D", String.format("total: %d, current: %d, ratio: %f",
+                        appBarLayout.getTotalScrollRange(), verticalOffset, ratio));
             }
         });
     }
